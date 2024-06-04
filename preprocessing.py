fa06d09dde2b2d4a2867514dd6e94d8e6b8fbb31
francescaiaconis <fraiac99@gmail.com>
2024-06-04 08:21:39 +0200

    preprocessing definitivo

--- preprocessing.py
+++ preprocessing.py
@@ -8,14 +8,17 @@ historical_stocks = pd.read_csv("../../Downloads/historical_stock_prices.csv/his
 print(historical_stock_prices.head())
 print(historical_stocks.head())
 
+historical_stocks=historical_stocks[historical_stocks["ticker"].isin(historical_stock_prices["ticker"])]
 # Pulizia dei dati: rimuovere le righe con valori nulli
-cleaned_prices = historical_stock_prices.dropna()
-cleaned_stocks = historical_stocks.dropna()
+cleaned_prices = historical_stock_prices.fillna("N/A")
+cleaned_stocks = historical_stocks.fillna("N/A")
 
 # Unire i dataset sulla colonna 'ticker'
-merged_data = pd.merge(cleaned_prices, cleaned_stocks, on="ticker")
+merged_data = pd.merge(cleaned_prices, cleaned_stocks, on="ticker", how="inner",validate="many_to_one")
+merged_data=merged_data.fillna("N/A")
 
 # Salvataggio del dataset pulito
 merged_data.to_csv("../../Downloads/historical_stock_prices.csv/merged_data.csv", index=False)
-
+print(merged_data.head())
+      
 print("Preprocessing completato e dati salvati in 'path/to/cleaned_stock_data.csv'")

8f9af83ec2a0ff0eb53f8d7102887523dbd3e393
francescaiaconis <fraiac99@gmail.com>
2024-06-21 14:33:29 +0200

    job2 hive quasi perfwetto

--- job-2/hive/job2.sql
+++ job-2/hive/job2.sql
@@ -28,84 +28,101 @@ SELECT
     ticker,
     stock_open,
     stock_close,
-    low,
-    high,
     volume,
     year(stock_date) AS stock_year,
-    stock_date,
-    exchange_name,
-    stock_name,
     sector,
     industry
 FROM
     stock_data;
 
-    
-WITH aggregated_data AS (
-    SELECT 
+-- Calcolo della variazione percentuale per ciascun ticker
+DROP VIEW IF EXISTS stock_data_with_percent_change;
+
+CREATE VIEW stock_data_with_percent_change AS
+SELECT
+    sector,
+    industry,
+    stock_year,
+    ticker,
+    stock_open,
+    stock_close,
+    volume,
+    ((stock_close - stock_open) / stock_open) * 100 AS percent_change
+FROM
+    stock_data_with_year;
+
+-- Aggregazione dei dati per (sector, industry, year)
+DROP VIEW IF EXISTS aggregated_data;
+
+CREATE VIEW aggregated_data AS
+SELECT
+    sector,
+    industry,
+    stock_year,
+    SUM(stock_open) AS sum_open_price,
+    SUM(stock_close) AS sum_close_price,
+    SUM(volume) AS sum_volume,
+    MAX(percent_change) AS max_percent_change,
+    MAX(volume) AS max_volume
+FROM
+    stock_data_with_percent_change
+GROUP BY
+    sector, industry, stock_year;
+
+-- Determinazione del ticker con la massima variazione percentuale
+DROP VIEW IF EXISTS max_percent_ticker;
+
+CREATE VIEW max_percent_ticker AS
+SELECT
+    sector,
+    industry,
+    stock_year,
+    percent_change,
+    ticker,
+    ROW_NUMBER() OVER (PARTITION BY sector, industry, stock_year ORDER BY percent_change DESC) AS rank
+FROM
+    stock_data_with_percent_change;
+
+-- Determinazione del ticker con il massimo volume
+DROP VIEW IF EXISTS max_volume_ticker;
+
+CREATE VIEW max_volume_ticker AS
+SELECT
+    sector,
+    industry,
+    stock_year,
+    total_volume,
+    ticker,
+    ROW_NUMBER() OVER (PARTITION BY sector, industry, stock_year ORDER BY total_volume DESC) AS rank
+FROM
+    (
+    SELECT
         sector,
         industry,
         stock_year,
         ticker,
-        stock_open,
-        stock_close,
-        SUM(volume) as total_volume
-    FROM stock_data_with_year
-    GROUP BY sector, industry, stock_year, ticker, stock_open, stock_close
-),
-industry_totals AS (
-    SELECT
-        sector,
-        industry,
-        stock_year,
-        SUM(stock_open) as total_open_price,
-        SUM(stock_close) as total_close_price,
-        SUM(total_volume) as total_volume
-    FROM aggregated_data
-    GROUP BY sector, industry, stock_year
-),
-industry_percentage_change AS (
-    SELECT
-        sector,
-        industry,
-        stock_year,
-        (total_close_price - total_open_price) / total_open_price * 100 as percentage_change
-    FROM industry_totals
-),
-max_percentage_ticker AS (
-    SELECT
-        sector,
-        industry,
-        stock_year,
-        ticker,
-        (stock_close - stock_open) / stock_open * 100 as percentage_change,
-        ROW_NUMBER() OVER (PARTITION BY sector, industry, stock_year ORDER BY (stock_close - stock_open) / stock_open * 100 DESC) as rank
-    FROM aggregated_data
-),
-max_volume_ticker AS (
-    SELECT
-        sector,
-        industry,
-        stock_year,
-        ticker,
-        total_volume,
-        ROW_NUMBER() OVER (PARTITION BY sector, industry, stock_year ORDER BY total_volume DESC) as rank
-    FROM aggregated_data
-)
-SELECT 
-    ipc.sector,
-    ipc.industry,
-    ipc.stock_year,
-    ROUND(ipc.percentage_change, 2) as industry_percentage_change,
-    mpt.ticker as max_percentage_ticker,
-    ROUND(mpt.percentage_change, 2) as max_percentage,
-    mvt.ticker as max_volume_ticker,
-    mvt.total_volume
-FROM 
-    industry_percentage_change ipc
-JOIN 
-    max_percentage_ticker mpt ON ipc.sector = mpt.sector AND ipc.industry = mpt.industry AND ipc.stock_year = mpt.stock_year AND mpt.rank = 1
-JOIN 
-    max_volume_ticker mvt ON ipc.sector = mvt.sector AND ipc.industry = mvt.industry AND ipc.stock_year = mvt.stock_year AND mvt.rank = 1
-ORDER BY 
-    ipc.sector, ipc.industry, ipc.stock_year;
+        SUM(volume) AS total_volume
+    FROM
+        stock_data_with_year
+    GROUP BY
+        sector, industry, stock_year, ticker
+    ) t;
+
+-- Calcolo della variazione percentuale dell'industria e unione dei dati finali
+SELECT
+    ad.sector,
+    ad.industry,
+    ad.stock_year,
+    ROUND(((ad.sum_close_price - ad.sum_open_price) / ad.sum_open_price) * 100, 2) AS industry_percent_change,
+    mpt.ticker AS ticker_max_change,
+    ROUND(mpt.percent_change, 2) AS max_percent_change,
+    mvt.ticker AS ticker_max_volume,
+    mvt.total_volume AS max_volume
+FROM
+    aggregated_data ad
+JOIN
+    max_percent_ticker mpt ON ad.sector = mpt.sector AND ad.industry = mpt.industry AND ad.stock_year = mpt.stock_year AND mpt.rank = 1
+JOIN
+    max_volume_ticker mvt ON ad.sector = mvt.sector AND ad.industry = mvt.industry AND ad.stock_year = mvt.stock_year AND mvt.rank = 1
+ORDER BY
+    ad.sector, ad.industry, ad.stock_year;
